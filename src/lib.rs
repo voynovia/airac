62d23e6acbe31439c521f3c612af1eb53700c811
Igor Voynov <igor@voynov.dev>
2020-02-12 23:48:31 +0300

    value to u16

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,11 @@ pub struct Airac {
     pub effective: NaiveDate,
     pub year: u16,
     pub ordinal: u8,
-    pub value: String
+    pub value: u16
 }
 
 impl Airac {
 
-//    fn cycle() -> Duration { Duration::weeks(1) }
     fn epoch() -> NaiveDate { NaiveDate::from_ymd(1901, 1, 10) }
 
     pub fn from_date_str(date: &str, week_cycle: i64) -> Airac {
@@ -37,7 +36,8 @@ impl Airac {
     fn from_date(effective: NaiveDate, duration: Duration) -> Airac {
         let year = effective.year() as u16;
         let ordinal = ((effective.ordinal()-1)/duration.num_days() as u32 + 1) as u8;
-        Airac { effective, year, ordinal, value: format!("{:02}{:02}", year%100, ordinal) }
+        let value = format!("{:02}{:02}", year%100, ordinal).parse::<u16>().expect("parse error");
+        Airac { effective, year, ordinal, value }
     }
 
     fn get_identifiers(yyoo: &str) -> Result<(u16, u8), AiracError> {
@@ -61,7 +61,7 @@ mod tests {
 
     struct AiracTest {
         date: String,
-        value: String
+        value: u16
     }
 
     #[test]
@@ -97,41 +97,41 @@ mod tests {
     #[test]
     fn from_date_month() {
         let mut airac_tests: Vec<AiracTest> = Vec::new();
-        airac_tests.push(AiracTest { date: "1998-01-29".to_string(), value: "9802".to_string() });
-        airac_tests.push(AiracTest { date: "2003-01-23".to_string(), value: "0301".to_string() });
+        airac_tests.push(AiracTest { date: "1998-01-29".to_string(), value: 9802 });
+        airac_tests.push(AiracTest { date: "2003-01-23".to_string(), value: 0301 });
 
-        airac_tests.push(AiracTest { date: "2004-01-21".to_string(), value: "0313".to_string() });
-        airac_tests.push(AiracTest { date: "2004-01-22".to_string(), value: "0401".to_string() });
+        airac_tests.push(AiracTest { date: "2004-01-21".to_string(), value: 0313 });
+        airac_tests.push(AiracTest { date: "2004-01-22".to_string(), value: 0401 });
 
-        airac_tests.push(AiracTest { date: "2005-01-19".to_string(), value: "0413".to_string() });
-        airac_tests.push(AiracTest { date: "2005-01-20".to_string(), value: "0501".to_string() });
+        airac_tests.push(AiracTest { date: "2005-01-19".to_string(), value: 0413 });
+        airac_tests.push(AiracTest { date: "2005-01-20".to_string(), value: 0501 });
 
-        airac_tests.push(AiracTest { date: "2006-01-18".to_string(), value: "0513".to_string() });
-        airac_tests.push(AiracTest { date: "2006-01-19".to_string(), value: "0601".to_string() });
+        airac_tests.push(AiracTest { date: "2006-01-18".to_string(), value: 0513 });
+        airac_tests.push(AiracTest { date: "2006-01-19".to_string(), value: 0601 });
 
-        airac_tests.push(AiracTest { date: "2007-01-17".to_string(), value: "0613".to_string() });
-        airac_tests.push(AiracTest { date: "2007-01-18".to_string(), value: "0701".to_string() });
+        airac_tests.push(AiracTest { date: "2007-01-17".to_string(), value: 0613 });
+        airac_tests.push(AiracTest { date: "2007-01-18".to_string(), value: 0701 });
 
-        airac_tests.push(AiracTest { date: "2008-01-16".to_string(), value: "0713".to_string() });
-        airac_tests.push(AiracTest { date: "2008-01-17".to_string(), value: "0801".to_string() });
+        airac_tests.push(AiracTest { date: "2008-01-16".to_string(), value: 0713 });
+        airac_tests.push(AiracTest { date: "2008-01-17".to_string(), value: 0801 });
 
-        airac_tests.push(AiracTest { date: "2009-01-14".to_string(), value: "0813".to_string() });
-        airac_tests.push(AiracTest { date: "2009-01-15".to_string(), value: "0901".to_string() });
+        airac_tests.push(AiracTest { date: "2009-01-14".to_string(), value: 0813 });
+        airac_tests.push(AiracTest { date: "2009-01-15".to_string(), value: 0901 });
 
-        airac_tests.push(AiracTest { date: "2010-01-13".to_string(), value: "0913".to_string() });
-        airac_tests.push(AiracTest { date: "2010-01-14".to_string(), value: "1001".to_string() });
+        airac_tests.push(AiracTest { date: "2010-01-13".to_string(), value: 0913 });
+        airac_tests.push(AiracTest { date: "2010-01-14".to_string(), value: 1001 });
 
-        airac_tests.push(AiracTest { date: "2011-01-12".to_string(), value: "1013".to_string() });
-        airac_tests.push(AiracTest { date: "2011-01-13".to_string(), value: "1101".to_string() });
+        airac_tests.push(AiracTest { date: "2011-01-12".to_string(), value: 1013 });
+        airac_tests.push(AiracTest { date: "2011-01-13".to_string(), value: 1101 });
 
-        airac_tests.push(AiracTest { date: "2012-01-11".to_string(), value: "1113".to_string() });
-        airac_tests.push(AiracTest { date: "2012-01-12".to_string(), value: "1201".to_string() });
+        airac_tests.push(AiracTest { date: "2012-01-11".to_string(), value: 1113 });
+        airac_tests.push(AiracTest { date: "2012-01-12".to_string(), value: 1201 });
 
-        airac_tests.push(AiracTest { date: "2013-01-09".to_string(), value: "1213".to_string() });
-        airac_tests.push(AiracTest { date: "2013-01-10".to_string(), value: "1301".to_string() });
+        airac_tests.push(AiracTest { date: "2013-01-09".to_string(), value: 1213 });
+        airac_tests.push(AiracTest { date: "2013-01-10".to_string(), value: 1301 });
 
-        airac_tests.push(AiracTest { date: "2020-01-01".to_string(), value: "1913".to_string() });
-        airac_tests.push(AiracTest { date: "2020-01-02".to_string(), value: "2001".to_string() });
+        airac_tests.push(AiracTest { date: "2020-01-01".to_string(), value: 1913 });
+        airac_tests.push(AiracTest { date: "2020-01-02".to_string(), value: 2001 });
 
         for airac_test in airac_tests {
             let airac = Airac::from_date_str(airac_test.date.as_str(), 4);
